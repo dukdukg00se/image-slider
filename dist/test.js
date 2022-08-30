2a7e83f9011ce8fe7283171d1d923cc8c2834459
Will H <will.wkhu@gmail.com>
2022-08-29 23:24:30 -0700

    Add carousel indicator functionality
    Added code to highlight indicator when corresponding img shown/selected.

--- dist/test.js
+++ dist/test.js
@@ -1,21 +1,21 @@
 /* eslint-disable */
 
 
-
-
-
 const slider = document.getElementById('image-slider');
 const sliderWrapper = document.getElementById('slider-wrapper');
 const sliderBtns = slider.querySelectorAll('.slider-btn');
 const frames = slider.querySelectorAll('.slide-frame');
 
 // Chrome doesn't round px values
-// Leads to image bleeding sometimes when resizing
+// Leads to occasional image bleeding when resizing
 // Fixed by manually setting slider width to 70% vw and round to whole number
 slider.style.width = Math.round(window.innerWidth * .7) + 'px';
 
 let size = sliderWrapper.offsetWidth;
 let counter = 1;
+
+updateIndicator(counter);
+
 
 sliderWrapper.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
@@ -28,16 +28,19 @@ sliderBtns.forEach(btn => {
 
       counter++;
       sliderWrapper.style.transform = 'translateX('+ (-size * counter) + 'px)';
-      sliderWrapper.style.transition = 'transform .5s';
+      sliderWrapper.style.transition = 'transform .5s ease-in';
+
     }
 
     if (btn.textContent === 'arrow_back_ios') {
-      if (counter < 0) return;
+      if (counter < 1) return;
 
       counter--;
-      sliderWrapper.style.transition = 'transform .5s';
+      sliderWrapper.style.transition = 'transform .5s ease-in';
       sliderWrapper.style.transform = 'translateX('+ (-size * counter) + 'px)';
     }
+
+    updateIndicator(counter);
 
   })
 })
@@ -71,3 +74,46 @@ sliderWrapper.addEventListener('transitionend', () => {
 
 
 
+
+function updateIndicator(count) {
+  let imgIndicators = document.querySelectorAll('.dot');
+
+  hlIndicator(count);
+
+  imgIndicators.forEach(indicator => {
+
+    indicator.addEventListener('click', (e) => {
+
+      counter = e.target.dataset.img;
+
+      hlIndicator(counter);
+
+      sliderWrapper.style.transition = 'transform .5s';
+      sliderWrapper.style.transform = 'translateX('+ (-size * counter) + 'px)';
+
+    });
+    
+  })
+}
+
+function hlIndicator(count) {
+  let imgIndicators = document.querySelectorAll('.dot');
+  imgIndicators.forEach(indicator => {
+    if (indicator.dataset.img == count) {
+      indicator.classList.add('displayed');
+    } else if (count === 6) {
+    
+      imgIndicators[0].classList.add('displayed');
+      imgIndicators[4].classList.remove('displayed')
+    
+    } else if (count === 0) {
+    
+      imgIndicators[4].classList.add('displayed');
+      imgIndicators[0].classList.remove('displayed');
+    
+    
+    } else {
+      indicator.classList.remove('displayed');
+    }
+  })
+}

501f6e890a268e499449ba91bd607f4db519841e
Will H <will.wkhu@gmail.com>
2022-09-08 23:13:59 -0700

    Save project
    Image slider working. Deleted test files, saved all other changes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import createSlider from './components/img-slider/slider';
+import { createSlider } from './components/img-slider/modules/view';
+import initSlider from './components/img-slider/modules/controller';
+
+/**
+ * Use to import slider images
+ * More on importAll(), require.context: https://webpack.js.org/guides/dependency-management/#require-context
+ * @param {*} r - request
+ * @returns - images obj w/ keys edited to remove "./" */
+function importAll(r) {
+  const images = {};
+
+  r.keys().forEach((item) => {
+    images[item.replace('./', '')] = r(item);
+  });
+
+  return images;
+}
+const imgs = importAll(
+  // Create own context
+  // Get image files from "./images" dir
+  require.context(
+    './components/img-slider/images',
+    false,
+    /\.(png|jpe?g|webp|svg)$/
+  )
+);
 
 const content = document.getElementById('content');
-content.append(createSlider());
+content.append(createSlider(imgs));
+initSlider(imgs);

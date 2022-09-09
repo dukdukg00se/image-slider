9e82119c055b7d8ccf432acd599cf55202f40c4a
Will H <will.wkhu@gmail.com>
2022-09-08 23:00:36 -0700

    Clean and comment code

--- src/components/img-slider/modules/controller.js
+++ src/components/img-slider/modules/controller.js
@@ -1,5 +1,6 @@
 import * as view from './view';
 
+// This module initializes the slider and controls slider functions
 const initSlider = (imgsObj) => {
   const sliderBtns = document.querySelectorAll('.slider-btn');
   const sliderWrapper = document.getElementById('slider-wrapper');
@@ -10,6 +11,9 @@ const initSlider = (imgsObj) => {
 
   const sliderController = (e) => {
     transition = 'transform .5s ease-in';
+
+    // For auto scrolling and slider btns click
+    // Add transitionend listener to loop slides
     if (!e || e.target.classList.contains('slider-btn')) {
       if (!e) {
         slideNmbr += 1;
@@ -20,10 +24,16 @@ const initSlider = (imgsObj) => {
         if (slideNmbr < 1) return;
         slideNmbr -= 1;
       }
+
       sliderWrapper.addEventListener('transitionend', sliderController);
     } else if (e.target.classList.contains('dot')) {
       slideNmbr = +e.target.dataset.img;
     } else if (e.type === 'transitionend') {
+      /**
+       * Check slideNmbr at end of transition
+       * Reset slideNmbr when greater/less than nmbr of imgs
+       * This "loops" the slides when scrolling through
+       */
       if (slideNmbr > nmbrOfImgs || slideNmbr < 1) {
         transition = 'none';
         if (slideNmbr > nmbrOfImgs) {
@@ -33,19 +43,19 @@ const initSlider = (imgsObj) => {
         }
       }
     }
+
     view.updateDisplay(slideNmbr, transition);
   };
 
-  view.highlight(slideNmbr);
+  // Add "displayed" class to highlightDot indicator first
+  // This class then used to show the correct slide on start
+  view.highlightDot(slideNmbr);
   view.setFrame();
   setInterval(sliderController, 4000);
-
   window.addEventListener('resize', view.setFrame);
-
   sliderBtns.forEach((btn) => {
     btn.addEventListener('click', sliderController);
   });
-
   indicators.forEach((dot) => {
     dot.addEventListener('click', sliderController);
   });
